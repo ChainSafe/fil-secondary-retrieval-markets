39b4e1681290aeb179ea9ebb7f3be25455421278
David Ansermino <david@chainsafe.io>
2020-07-01 11:33:23 -0400

    Client response handler (#32)
    - Adds `client.HandlerProviderStream` which can later be registered with the libp2p host
    - Adds `host.RegisterStreamHandler`
    - Moves libp2p protocol defn to `shared`

--- client/client.go
+++ client/client.go
@@ -4,26 +4,37 @@
 package client
 
 import (
+	"bufio"
 	"context"
+
 	"encoding/json"
 
 	"github.com/ChainSafe/fil-secondary-retrieval-markets/shared"
 	"github.com/ipfs/go-cid"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p-core/network"
 )
+
+var log = logging.Logger("client")
 
 type Client struct {
 	host Host
 }
 
 func NewClient(host Host) *Client {
-	return &Client{host: host}
+	c := &Client{host: host}
+
+	// Register handler for provider responses
+	c.host.RegisterStreamHandler(shared.RetrievalProtocolID, c.HandleProviderStream)
+
+	return c
 }
 
 // SubmitQuery encodes a query a submits it to the network to be gossiped
 func (c *Client) SubmitQuery(ctx context.Context, cid cid.Cid) error {
 	query := shared.Query{
-		PayloadCID: cid,
-		Client:     c.host.MultiAddrs(),
+		PayloadCID:  cid,
+		ClientAddrs: c.host.MultiAddrs(),
 	}
 	bz, err := json.Marshal(query)
 	if err != nil {
@@ -36,4 +47,31 @@ func (c *Client) SubmitQuery(ctx context.Context, cid cid.Cid) error {
 	}
 
 	return nil
+}
+
+// HandleProviderStream reads the first message and calls HandleProviderResponse
+// Note: implements the libp2p StreamHandler interface
+func (c *Client) HandleProviderStream(s network.Stream) {
+	// Read message from stream
+	buf := bufio.NewReader(s)
+	bz, err := buf.ReadBytes('\n')
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	c.HandleProviderResponse(bz)
+}
+
+// HandleProviderResponse is called to handle a QueryResponse from a provider
+func (c *Client) HandleProviderResponse(msg []byte) {
+	var response shared.QueryResponse
+	err := json.Unmarshal(msg, &response)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	// TODO: Make use of message
+	log.Infof("Response received: %+v", msg)
 }

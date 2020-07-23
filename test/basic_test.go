225d6451f5a7739679901c7e9c11f85690f18842
noot <36753753+noot@users.noreply.github.com>
2020-07-23 10:17:28 -0400

    implement 3 client, 3 provider test and multi-provider test (#57)

--- test/basic_test.go
+++ test/basic_test.go
@@ -6,6 +6,8 @@ package test
 import (
 	"context"
 	"math/big"
+	"os"
+	"sort"
 	"testing"
 	"time"
 
@@ -15,15 +17,17 @@ import (
 	"github.com/ChainSafe/fil-secondary-retrieval-markets/provider"
 	"github.com/ChainSafe/fil-secondary-retrieval-markets/shared"
 	block "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
 	logging "github.com/ipfs/go-log/v2"
 	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
 
 	"github.com/stretchr/testify/require"
 )
 
-var testTimeout = time.Second * 15
+var testTimeout = time.Second * 30
 
 func newTestNetwork(t *testing.T) *network.Network {
 	ctx := context.Background()
@@ -32,6 +36,11 @@ func newTestNetwork(t *testing.T) *network.Network {
 
 	net, err := network.NewNetwork(h)
 	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		require.NoError(t, net.Stop())
+		require.NoError(t, h.Close())
+	})
 	return net
 }
 
@@ -54,23 +63,32 @@ func (bt *basicTester) handleResponse(resp shared.QueryResponse) {
 	bt.respCh <- &resp
 }
 
-func TestBasic(t *testing.T) {
+func TestMain(m *testing.M) {
 	err := logging.SetLogLevel("client", "debug")
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 	err = logging.SetLogLevel("provider", "debug")
-	require.NoError(t, err)
+	if err != nil {
+		panic(err)
+	}
 
+	time.Sleep(time.Second * 10)
+	os.Exit(m.Run())
+}
+
+func TestBasic(t *testing.T) {
 	pnet := newTestNetwork(t)
 	cnet := newTestNetwork(t)
 	bs := newTestBlockstore()
 
-	err = pnet.Connect(cnet.AddrInfo())
+	err := pnet.Connect(cnet.AddrInfo())
 	require.NoError(t, err)
 
 	p := provider.NewProvider(pnet, bs, cache.NewMockCache(0))
 	c := client.NewClient(cnet)
 
-	// ad data block to blockstore
+	// add data block to blockstore
 	b := block.NewBlock([]byte("noot"))
 	testCid := b.Cid()
 	err = bs.Put(b)
@@ -79,18 +97,10 @@ func TestBasic(t *testing.T) {
 	// start provider
 	err = p.Start()
 	require.NoError(t, err)
-	defer func() {
-		err = p.Stop()
-		require.NoError(t, err)
-	}()
 
 	// start client
 	err = c.Start()
 	require.NoError(t, err)
-	defer func() {
-		err = c.Stop()
-		require.NoError(t, err)
-	}()
 
 	// subscribe to responses
 	bt := newBasicTester()
@@ -117,4 +127,181 @@ func TestBasic(t *testing.T) {
 	case <-time.After(testTimeout):
 		t.Fatal("did not receive response")
 	}
+}
+
+func TestMulti(t *testing.T) {
+	numClients := 3
+	numProviders := 3
+	data := [][]byte{
+		[]byte("noot"),
+		[]byte("was"),
+		[]byte("here"),
+	}
+	cids := make([]cid.Cid, len(data))
+
+	clients := make([]*client.Client, numClients)
+	providers := make([]*provider.Provider, numProviders)
+	blockstores := make([]blockstore.Blockstore, numProviders)
+	cnets := make([]*network.Network, numClients)
+	pnets := make([]*network.Network, numProviders)
+
+	// create and start clients
+	for i := 0; i < numClients; i++ {
+		net := newTestNetwork(t)
+		c := client.NewClient(net)
+
+		err := c.Start()
+		require.NoError(t, err)
+
+		clients[i] = c
+		cnets[i] = net
+	}
+
+	// create and start providers
+	for i := 0; i < numProviders; i++ {
+		net := newTestNetwork(t)
+		bs := newTestBlockstore()
+		p := provider.NewProvider(net, bs, cache.NewMockCache(0))
+
+		err := p.Start()
+		require.NoError(t, err)
+
+		providers[i] = p
+		blockstores[i] = bs
+		pnets[i] = net
+	}
+
+	// connect clients to providers
+	for _, cnet := range cnets {
+		for _, pnet := range pnets {
+			err := pnet.Connect(cnet.AddrInfo())
+			require.NoError(t, err)
+		}
+	}
+
+	// add data to blockstores
+	for i, bs := range blockstores {
+		// add data block to blockstore
+		b := block.NewBlock(data[i])
+		cids[i] = b.Cid()
+		err := bs.Put(b)
+		require.NoError(t, err)
+	}
+
+	// each client queries for a different cid
+	for i, c := range clients {
+		// subscribe to responses
+		bt := newBasicTester()
+		unsubscribe := c.SubscribeToQueryResponses(bt.handleResponse, cids[i])
+		defer unsubscribe()
+
+		// submit query
+		err := c.SubmitQuery(context.Background(), cids[i])
+		require.NoError(t, err)
+
+		// assert response was received
+		expected := &shared.QueryResponse{
+			PayloadCID:              cids[i],
+			Provider:                pnets[i].PeerID(),
+			Total:                   big.NewInt(0),
+			PaymentInterval:         0,
+			PaymentIntervalIncrease: 0,
+		}
+
+		select {
+		case resp := <-bt.respCh:
+			require.NotNil(t, resp)
+			require.Equal(t, expected, resp)
+		case <-time.After(testTimeout):
+			t.Fatal("did not receive response")
+		}
+	}
+}
+
+func TestMultiProvider(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping TestMultiProvider")
+	}
+
+	pnet0 := newTestNetwork(t)
+	pnet1 := newTestNetwork(t)
+	cnet := newTestNetwork(t)
+	bs0 := newTestBlockstore()
+	bs1 := newTestBlockstore()
+
+	err := pnet0.Connect(cnet.AddrInfo())
+	require.NoError(t, err)
+	err = pnet1.Connect(cnet.AddrInfo())
+	require.NoError(t, err)
+	err = pnet1.Connect(pnet0.AddrInfo())
+	require.NoError(t, err)
+
+	require.GreaterOrEqual(t, len(pnet0.Peers()), 2)
+	require.GreaterOrEqual(t, len(pnet1.Peers()), 2)
+	require.GreaterOrEqual(t, len(cnet.Peers()), 2)
+
+	p0 := provider.NewProvider(pnet0, bs0, cache.NewMockCache(0))
+	p1 := provider.NewProvider(pnet1, bs1, cache.NewMockCache(0))
+	c := client.NewClient(cnet)
+
+	// add data to both providers's blockstores
+	b := block.NewBlock([]byte("noot"))
+	testCid := b.Cid()
+	err = bs0.Put(b)
+	require.NoError(t, err)
+	err = bs1.Put(b)
+	require.NoError(t, err)
+
+	// start providers and client
+	err = p0.Start()
+	require.NoError(t, err)
+
+	err = p1.Start()
+	require.NoError(t, err)
+
+	err = c.Start()
+	require.NoError(t, err)
+
+	// query for CID, should receive responses from both providers
+	bt := newBasicTester()
+	unsubscribe := c.SubscribeToQueryResponses(bt.handleResponse, testCid)
+	defer unsubscribe()
+
+	// submit query
+	err = c.SubmitQuery(context.Background(), testCid)
+	require.NoError(t, err)
+
+	// assert response was received
+	expected := &shared.QueryResponse{
+		PayloadCID:              testCid,
+		Total:                   big.NewInt(0),
+		PaymentInterval:         0,
+		PaymentIntervalIncrease: 0,
+	}
+
+	receivedFrom := []peer.ID{}
+
+	for i := 0; i < 2; i++ {
+		select {
+		case resp := <-bt.respCh:
+			require.NotNil(t, resp)
+			respProvider := resp.Provider
+			resp.Provider = ""
+			require.Equal(t, expected, resp)
+			t.Log("received from", respProvider)
+			receivedFrom = append(receivedFrom, respProvider)
+		case <-time.After(testTimeout):
+			t.Fatal("did not receive response")
+		}
+	}
+
+	// assert response was received from providers 0 and 1
+	expectedResponders := []peer.ID{pnet0.PeerID(), pnet1.PeerID()}
+	sort.Slice(expectedResponders, func(i, j int) bool {
+		return expectedResponders[i].String() < expectedResponders[j].String()
+	})
+	sort.Slice(receivedFrom, func(i, j int) bool {
+		return receivedFrom[i].String() < receivedFrom[j].String()
+	})
+	require.Equal(t, expectedResponders, receivedFrom)
 }
